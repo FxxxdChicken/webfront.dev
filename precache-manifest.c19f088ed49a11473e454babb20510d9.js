cadcbb5614b46a647bd1e1fdf2177de9b019cc44
bugtype <bugtype.kr@gmail.com>
2019-05-12 13:08:46 +0900

    Updates

---
+++ precache-manifest.c19f088ed49a11473e454babb20510d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee8647433504b6ff9cda",
+    "url": "/webfront.dev/static/css/main.99376ec9.chunk.css"
+  },
+  {
+    "revision": "ee8647433504b6ff9cda",
+    "url": "/webfront.dev/static/js/main.ee864743.chunk.js"
+  },
+  {
+    "revision": "93c92d6535fd84c3604c",
+    "url": "/webfront.dev/static/js/runtime~main.93c92d65.js"
+  },
+  {
+    "revision": "c8b0afa471fea66faa6e",
+    "url": "/webfront.dev/static/js/2.c8b0afa4.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/webfront.dev/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8fc49089e8c1ddc768d383a72f620c82",
+    "url": "/webfront.dev/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/webfront.dev/precache-manifest.dea84af66cb99d4b9edd1435cd9e8850.js"
+  "/webfront.dev/precache-manifest.c19f088ed49a11473e454babb20510d9.js"
 );
 
 workbox.clientsClaim();
